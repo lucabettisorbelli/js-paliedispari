2be15791ce72667950732d776535521a5cfa5fe7
luca betti sorbelli <luca betti sorbelli>
2023-04-23 12:28:08 +0200

    fine esercizio (da rivedere)

--- js/main.js
+++ js/main.js
@@ -1,48 +1,61 @@
 
 // esercizio palindromo
 
-// let parola = prompt("inserisci una parola");
-// function isPalindromo () {
-//     let stringaParola = parola.split(""); // trasforma stringa in array
-//     let StringaAlContrario = stringaParola.reverse(); // rovescia l'array
-//     let parolaAlContrario = StringaAlContrario.join(""); // "unisce elementi array"
+    // let parolaUtente = prompt("Iserisci una parola");
 
-//     if (parolaAlContrario === parola) {
-//         console.log(`la parola ${parolaAlContrario} è palindroma`);
-//     } else {
-//         console.log("la parola è sbagliata");
-//     }
+    // let reverseWord = "";
+    // for (let i = parolaUtente.length -1; i >= 0; i--) {
+    //     reverseWord += parolaUtente[i];
+    // }
 
-// }
+    // if (parolaUtente === reverseWord) {
+    //     console.log(`${parolaUtente} è palindroma`);
+    // } else {
+    //     console.log(`${parolaUtente} non è palindroma`);
+    // };
 
-// isPalindromo ();
 
-// esercizio pari o dispari
+//esercizio pari o dispari
+
 let numeroUtente = parseInt(prompt("inserisci un numero da 1 a 5"));
 let pariDispariUtente = prompt("scegli pari o dispari");
-let numeroOk;
 
-if (numeroUtente <= 5 && numeroUtente !== 0) {
-    let numeroOk = numeroUtente;
-    console.log(`il numero ${numeroOk} è corretto`)
+if (numeroUtente <= 5 && numeroUtente >=1) {
+    console.log(`il numero ${numeroUtente} è corretto`);
+    
+    if ( numeroUtente % 2 == 0) {
+        console.log("hai scelto un numero pari");
+    } else {
+        console.log("hai scelto un numero dispari");
+        
+    } 
+    } 
+
+function numeroRandom (min, max) {
+    
+
+    let risultato = Math.floor(Math.random() * (max - min + 1) + min);
+
+    return risultato;
+    
+}
+
+
+let mioNumero = numeroRandom(1,5);
+console.log(`il mio numero generato è ${mioNumero}`);
+
+let sommaNumeri = numeroUtente + mioNumero;
+console.log(sommaNumeri);
+
+if (sommaNumeri % 2 == 0 && pariDispariUtente == "pari") {
+    console.log("ha vinto l'utente");
+} else if (sommaNumeri % 2 == 1 && pariDispariUtente == "dispari") {
+    console.log("ha vinto l'utente");
 } else {
-    console.log(`il tuo numero ${numeroUtente} non è corretto`);
-}
-
-let numeroPariUtente;
-let numeroDispariUtente;
-
-if ( numeroOk % 2 == 0) {
-    let numeroOk = numeroPariUtente;
-    console.log("hai scelto un numero pari")
-} else {
-    numeroOk = numeroDispariUtente;
-    console.log("hai scelto un numero dispari")
-
-}
-
-function numeroRandom () {
+    console.log ("ho vinto io!");
+} ;
 
 
 
-}
+
+
